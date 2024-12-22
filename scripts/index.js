126f97e049330d17e6963d0dfe90bd736168e9f1
Vasilii Brelgin <vbrelgin@gmail.com>
2024-12-22 22:12:27 +0300

    added correct JS functions for project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,143 @@
-// @todo: Темплейт карточки
+// ----- Профиль модалки
+const allPopups = document.querySelectorAll('.popup');
+const profilePopup = document.querySelector('.popup_type_edit');
+const cardPopup = document.querySelector('.popup_type_new-card');
+const imagePopup = document.querySelector('.popup_type_image');
 
-// @todo: DOM узлы
+// ----- Профиль
+const inputName = document.querySelector('.popup__input_type_name');
+const inputAbout = document.querySelector('.popup__input_type_description');
 
-// @todo: Функция создания карточки
+const profileInfoName = document.querySelector('.profile__title');
+const profileInfoAbout = document.querySelector('.profile__description');
 
-// @todo: Функция удаления карточки
+const profileEditButton = document.querySelector('.profile__edit-button');
 
-// @todo: Вывести карточки на страницу
+// ----- Добавление карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
+const inputCardName = document.querySelector('.popup__input_type_card-name');
+const inputUrl = document.querySelector('.popup__input_type_url');
+
+const cardAddButton = document.querySelector('.profile__add-button');
+const cardsContainer = document.querySelector('.places__list');
+
+// ----- Просмотр карточки
+const imagePopupImage = document.querySelector('.popup__image');
+const imagePopupCaption = document.querySelector('.popup__caption');
+
+
+// Функции для карточек
+function cardLikeAction(event) {
+    const likeButton = event.target.closest(".card__like-button")
+    likeButton.classList.toggle("card__like-button_is-active")
+}
+
+function deleteCard(event) {
+    const cardToDelete = event.target.closest(".card")
+    cardToDelete.remove()
+}
+
+
+// Основная функция для создания карточек
+function createCard(title, url) {
+    const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardsImg = newCard.querySelector('.card__image');
+    const cardsTitle = newCard.querySelector('.card__title')
+    const cardsBtnRemove = newCard.querySelector('.card__delete-button');
+    const cardsLike = newCard.querySelector('.card__like-button');
+
+    cardsImg.src = url;
+    cardsImg.alt = title;
+    cardsTitle.textContent = title;
+
+    // Обработчик нажатия кнопки лайка
+    cardsLike.addEventListener('click', (evt) => {cardLikeAction(evt)})
+
+    // Обработчик удаления карточки
+    cardsBtnRemove.addEventListener('click', (evt) => {deleteCard(evt)})
+
+    // Открытие карточки
+    cardsImg.addEventListener('click', () => {openImageModal(title, url)});
+
+    return newCard;
+}
+
+
+// Фун-ии открытия/закрытия модалок
+function openModal(popup) {
+    popup.classList.add('popup_is-opened');
+}
+
+function closeModal(popup) {
+    popup.classList.remove('popup_is-opened');
+}
+
+
+// Функции для модалки профиля
+function openProfileForm() {
+    inputName.value = profileInfoName.textContent;
+    inputAbout.value = profileInfoAbout.textContent;
+    openModal(profilePopup);
+}
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+    profileInfoName.textContent = inputName.value;
+    profileInfoAbout.textContent = inputAbout.value;
+    closeModal(profilePopup);
+}
+
+
+// Функции для модалки добавления карточки
+function openCardForm() {
+    inputCardName.value = "";
+    inputUrl.value = "";
+    openModal(cardPopup);
+}
+
+function handleCardFormSubmit(evt) {
+    evt.preventDefault();
+    const createdCard = createCard(inputCardName.value, inputUrl.value)
+    cardsContainer.prepend(createdCard)
+    closeModal(cardPopup);
+}
+
+
+// Функции для модалки просмотра карточки
+function openImageModal(title, url) {
+    imagePopupImage.src = url;
+    imagePopupImage.alt = title;
+    imagePopupCaption.textContent = title;
+    openModal(imagePopup);
+}
+
+
+// Вызывается сразу. Рендерит все карточки из cards.js
+function renderExistingCards() {
+    initialCards.forEach(card => {
+        let newCard = createCard(card.name, card.link)
+        cardsContainer.append(newCard)
+    })
+}
+
+
+// Открытие и сохранение профиля
+profileEditButton.addEventListener('click', openProfileForm)
+profilePopup.addEventListener('submit', handleProfileFormSubmit);
+
+// Открытие и добавление карточки
+cardAddButton.addEventListener('click', openCardForm);
+cardPopup.addEventListener('submit', handleCardFormSubmit);
+
+
+// Закрытие модалки
+allPopups.forEach((popup) => {
+    const closeButton = popup.querySelector('.popup__close');
+    closeButton.addEventListener('click', () => closeModal(popup));
+});
+
+// Добавляем для каждого поп-апа анимацию открытия/закрытия
+allPopups.forEach(popup => popup.classList.add('popup_is-animated'));
+
+renderExistingCards();
